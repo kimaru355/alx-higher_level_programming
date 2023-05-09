2ea731991ecec96ccf7584fc72c0df5b2bf89eb8
kimaru355 <emmanuelkimaru355@gmail.com>
2023-05-08 17:15:45 -0700

    string concatenation

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming"
-str = str[-27:]
-str += " with Python"
+str = "Python is an interpreted, interactive, object-oriented programming\
+ and high-level with readable syntax"
+str = str[39:66] + str[81:87] + str[:6]
 print(str)
