ab45ae87e2835650abfca4ac0a9b3e439e935ff2
kimaru355 <emmanuelkimaru355@gmail.com>
2023-05-10 16:22:24 -0700

    combinations

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 for a in range(10):
     for b in range(10):
-        if a >= b:
-            continue
-        elif a == 8 and b == 9:
+        if a == 8 and b == 9:
             print(a, b, sep="")
-        else:
+        elif b > a:
             print(a, b, sep="", end=", ")
