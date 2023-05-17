d9104a7927509ed90f675b0d380b06529a5d856b
kimaru355 <emmanuelkimaru355@gmail.com>
2023-05-17 07:15:16 -0700

    ""

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    if my_string:
+    if my_string = "":
+        return my_string
+    else:
         my_string_copy = ""
         for a in my_string:
             if a == 'c' or a == 'C':
