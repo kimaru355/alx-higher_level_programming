4603bed13e1f73a70a084fda0578486ee383ce82
kimaru355 <emmanuelkimaru355@gmail.com>
2023-05-17 16:10:10 -0700

    not handle empty

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 
 def print_sorted_dictionary(a_dictionary):
-    if len(a_dictionary) == 0:
-        print()
-        return None
     my_keys = list(sorted(a_dictionary))
     for i in my_keys:
         print(f"{i}: {a_dictionary.get(i)}")
