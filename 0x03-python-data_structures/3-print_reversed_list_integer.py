87006526f68fdd7bbafc99684f057ca4d742710f
kimaru355 <emmanuelkimaru355@gmail.com>
2023-05-17 06:44:07 -0700

    handle empty list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
     a = len(my_list) - 1
     for i in range(len(my_list)):
         print("{:d}".format(my_list[a]))
