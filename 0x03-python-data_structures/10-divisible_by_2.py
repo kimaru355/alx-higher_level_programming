071f67f7a03edb3a064920c416dc305a62e8865c
kimaru355 <emmanuelkimaru355@gmail.com>
2023-05-17 11:34:54 -0700

    for x in list vs for x in range(len(list))

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,10 +2,10 @@
 
 def divisible_by_2(my_list=[]):
     new_list = []
-    for i in my_list:
-        if i == 0:
+    for i in range(len(my_list)):
+        if my_list[i] == 0:
             new_list.append(False)
-        elif i % 2 == 0:
+        elif my_list[i] % 2 == 0:
             new_list.append(True)
         else:
             new_list.append(False)
