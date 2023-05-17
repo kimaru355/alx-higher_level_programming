b90a8293d17792cb40cf138baeb83ce462618d81
kimaru355 <emmanuelkimaru355@gmail.com>
2023-05-17 06:48:10 -0700

    if not list or if len(list) == 0?

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    if len(my_list) == 0:
-        return None
-    a = len(my_list) - 1
-    for i in range(len(my_list)):
-        print("{:d}".format(my_list[a]))
-        a -= 1
+    if not my_list:
+        pass
+    else:
+        a = len(my_list) - 1
+        for i in range(len(my_list)):
+            print("{:d}".format(my_list[a]))
+            a -= 1
