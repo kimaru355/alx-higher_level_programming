0fa3d8397823c490d1a0018e1387917a8788f7f3
kimaru355 <emmanuelkimaru355@gmail.com>
2023-05-29 16:34:44 -0700

    do not use bare except

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,3 +1,16 @@
 #!/usr/bin/python3
 
 def safe_print_list_integers(my_list[], x=0):
+    if my_list != [] and x != 0:
+        a = 0
+        b = 0
+        for i in my_list:
+            a += 1
+            try:
+                print("{:d}".format(i), end="")
+                b += 1
+            except ValueError:
+                continue
+            if i == x:
+                break
+        return b
