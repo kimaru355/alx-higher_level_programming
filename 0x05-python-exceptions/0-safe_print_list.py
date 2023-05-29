8154e29cd626b902850080f382f7a3d3858a4689
kimaru355 <emmanuelkimaru355@gmail.com>
2023-05-29 15:30:08 -0700

    exception handling

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    if my_list != [] and x != 0:
+        a = 0
+        for i in my_list:
+            if a == x:
+                break
+            print_int(i)
+            a += 1
+        print()
+        return a
+
+
+def print_int(i):
+    try:
+        print("{:d}".format(i), end="")
+    except TypeError:
+        print_float(i)
+
+
+def print_float(i):
+    try:
+        print("{:f}".format(i), end="")
+    except TypeError:
+        print_str(i)
+
+
+def print_str(i):
+    try:
+        print("{:s".format(i), end="")
+    except TypeError:
+        print("", end="")
